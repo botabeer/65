0db15864981f6cf19c1abb9f964b3aa59b751a4f
عبير <alqaws151@gmail.com>
2025-12-25 20:43:06 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -10,9 +10,6 @@ from .category_letter_game import CategoryGame
 from .human_animal_plant_game import HumanAnimalGame
 from .iq_game import IqGame
 from .scramble_word_game import ScrambleGame
-from .word_color_game import WordColorGame
-from .roulette_game import RouletteGame
-from .seen_jeem_game import SeenJeemGame
 from .letter_game import LetterGame
 from .mafia_game import MafiaGame
 
@@ -20,6 +17,5 @@ __all__ = [
     'BaseGame', 'GuessGame', 'FastGame', 'CompatibilityGame',
     'SongGame', 'OppositeGame', 'ChainGame', 'LettersGame',
     'CategoryGame', 'HumanAnimalGame', 'IqGame', 'ScrambleGame',
-    'WordColorGame', 'RouletteGame', 'SeenJeemGame', 'LetterGame',
-    'MafiaGame'
+    'LetterGame', 'MafiaGame'
 ]
