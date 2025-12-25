d8eb72b2fd66e1a8650f154ebfdee0bb1ab6d737
عبير <alqaws151@gmail.com>
2025-12-25 14:16:03 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,13 +1,14 @@
+from .base_game import BaseGame
+from .guess_game import GuessGame
+from .fast_typing_game import FastGame
+from .compatibility_game import CompatibilityGame
 from .song_game import SongGame
 from .opposite_game import OppositeGame
 from .chain_words_game import ChainGame
-from .fast_typing_game import FastGame
 from .letters_words_game import LettersGame
 from .category_letter_game import CategoryGame
 from .human_animal_plant_game import HumanAnimalGame
-from .compatibility_game import CompatibilityGame
 from .iq_game import IqGame
-from .guess_game import GuessGame
 from .scramble_word_game import ScrambleGame
 from .word_color_game import WordColorGame
 from .roulette_game import RouletteGame
@@ -16,9 +17,9 @@ from .letter_game import LetterGame
 from .mafia_game import MafiaGame
 
 __all__ = [
-    'SongGame', 'OppositeGame', 'ChainGame', 'FastGame',
-    'LettersGame', 'CategoryGame', 'HumanAnimalGame',
-    'CompatibilityGame', 'IqGame', 'GuessGame', 'ScrambleGame',
+    'BaseGame', 'GuessGame', 'FastGame', 'CompatibilityGame',
+    'SongGame', 'OppositeGame', 'ChainGame', 'LettersGame',
+    'CategoryGame', 'HumanAnimalGame', 'IqGame', 'ScrambleGame',
     'WordColorGame', 'RouletteGame', 'SeenJeemGame', 'LetterGame',
     'MafiaGame'
 ]
