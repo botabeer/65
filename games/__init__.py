6d0182c578182b1ec4ecde973cc45232a3a1953b
عبير <alqaws151@gmail.com>
2025-12-25 21:48:45 +0300

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,1 +1,29 @@
+from games.guess_game import GuessGame
+from games.fast_typing_game import FastGame
+from games.compatibility_game import CompatibilityGame
+from games.song_game import SongGame
+from games.opposite_game import OppositeGame
+from games.chain_words_game import ChainGame
+from games.letters_words_game import LettersGame
+from games.category_letter_game import CategoryGame
+from games.human_animal_plant_game import HumanAnimalGame
+from games.iq_game import IqGame
+from games.scramble_word_game import ScrambleGame
+from games.letter_game import LetterGame
+from games.mafia_game import MafiaGame
 
+__all__ = [
+    'GuessGame',
+    'FastGame',
+    'CompatibilityGame',
+    'SongGame',
+    'OppositeGame',
+    'ChainGame',
+    'LettersGame',
+    'CategoryGame',
+    'HumanAnimalGame',
+    'IqGame',
+    'ScrambleGame',
+    'LetterGame',
+    'MafiaGame'
+]
